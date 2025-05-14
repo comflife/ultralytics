92f4666311b4b3f908547037c7b7e9d6e30d1ddd
comflife <okharry1@naver.com>
2025-05-14 19:50:00 +0900

    add something todo

---
+++ main.py
@@ -0,0 +1,16 @@
+from ultralytics import YOLO
+
+def freeze_layer(trainer):
+    model = trainer.model
+    num_freeze = 21
+    print(f"Freezing {num_freeze} layers")
+    freeze = [f'model.{x}.' for x in range(num_freeze)]  # layers to freeze 
+    for k, v in model.named_parameters(): 
+        v.requires_grad = True  # train all layers 
+        if any(x in k for x in freeze): 
+            print(f'freezing {k}') 
+            v.requires_grad = False 
+    print(f"{num_freeze} layers are freezed.")
+model=YOLO("yolov8s.pt")
+model.add_callback("on_train_start", freeze_layer)
+results = model.train(data="VOC.yaml", epochs=200, imgsz=640, batch=256)
